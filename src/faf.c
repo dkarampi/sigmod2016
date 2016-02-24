1252a2aefb699a8899c4fdc91782bd3b7159af8d
Dimitris Karampinas <dkarampin@gmail.com>
2016-02-24 04:18:10 +0100

    Minor progres. Added a small try & test file. Broken code.

--- src/faf.c
+++ src/faf.c
@@ -7,48 +7,42 @@
 #define TRUE !FALSE
 
 #define BUF_LEN		128
-#define MAX_V		(1 << 20) /* ~1M vertices */
+#define MAX_V		(1 << 21) /* ~2M vertices */
 #define E_PER_EPAGE	14
 
 typedef	uint32_t vid_t;
 
 typedef struct e_page
 {
-	/* header */
 	uint32_t num_e;
 	struct e_page *next;
 
-	/* payload */
-	vid_t vid[E_PER_EPAGE]; /* outgoing edges */
+	/* vertex ids correspoding to each outgoing edge */
+	vid_t vid[E_PER_EPAGE];
 } e_page_t;
 
-typedef struct vertex
-{
-	e_page_t *edges;
-} vertex_t;
-
-// http://www.cs.yale.edu/homes/aspnes/pinewiki/C(2f)Graphs.html
 typedef struct graph
 {
-	vertex_t *v;		/* vetrices */
+	e_page_t **v; /* each vertex points to a page of edges */
 	uint32_t num_v;
 } graph_t;
 
 graph_t *graph_create(const uint32_t n)
 {
-	graph_t *g = malloc(sizeof(g));
-
-	if (g == NULL) {
+	graph_t *g;
+	
+	if ((g = malloc(sizeof(graph_t))) == NULL) {
 		perror("malloc");
 		exit(EXIT_FAILURE);
 	}
 	
-	g->v = malloc(n * sizeof(vertex_t));
-	if (g->v == NULL) {
+	if ((g->v = malloc(n * sizeof(e_page_t *))) == NULL) {
 		perror("malloc");
 		exit(EXIT_FAILURE);
 	}
 
+	for (uint32_t i = 0; i < n; i++)
+		g->v[i] = NULL;
 	g->num_v = 0;
 
 	return g;
@@ -56,20 +50,23 @@ graph_t *graph_create(const uint32_t n)
 
 void graph_destroy(graph_t *g)
 {
+	for (uint32_t i = 0; i < g->num_v; i++)
+		free(g->v[i]);
+	free(g->v);
 	free(g);
 	g = NULL;
 }
 
 void graph_add_edge(graph_t *g, vid_t v1, vid_t v2)
 {
-	e_page_t *edges = g->v[v1]->edges;
+	e_page_t *edges = g->v[v1];
 
 	/* Check if edge exists already. XXX Consider removing this step. */
 	for (e_page_t *p = edges; p != NULL; p = p->next) {
 		/* Iterate over all outgoing edges of this page */
 		for (int i = 0; i < p->num_e; i++) {
-			if (vid[i] == v2) {
-				fprintnf(stdout, "edge %" PRIu32 +
+			if (p->vid[i] == v2) {
+				fprintf(stdout, "edge %" PRIu32
 						" --> %" PRIu32 " exists already\n", v1, v2);
 				return;
 			}
@@ -77,14 +74,21 @@ void graph_add_edge(graph_t *g, vid_t v1, vid_t v2)
 		p = p->next;
 	}
 
-	if (edges == NULL)
-
-	for () {
-
+	if (edges == NULL) {
+		/* v1 has no outgoing edges, add a new page */
+		e_page_t *p;
+		if ((p = malloc(sizeof(e_page_t))) == NULL) {
+			perror("malloc");
+			exit(EXIT_FAILURE);
+		}
+		p->vid[0] = v2;
+		p->num_e = 1;
+		p->next = NULL;
+		g->v[v1].edges = p;
+		return;
 	}
-	p = g->v[v1]->edges;
-	while (p !=NULL && ) {
-	}
+
+	e_page_t *p = NULL;
 	// if there is no empty spot, create a new page
 }
 
@@ -110,3 +114,5 @@ int main(void)
 
 	return 0;
 }
+
+// http://www.cs.yale.edu/homes/aspnes/pinewiki/C(2f)Graphs.html
---
+++ src/test.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUCKET_SIZE	10
+#define FALSE 0
+#define TRUE !FALSE
+
+typedef struct edge_bucket
+{
+	int outedges[5];
+} edge_bucket_t;
+
+typedef struct graph
+{
+	int num_of_vetrices;
+	edge_bucket_t **v;
+} graph_t;
+
+
+graph_t *graph_create(int n)
+{
+	int i;
+	graph_t *g;
+	
+	if ((g = malloc(sizeof(graph_t))) == NULL) {
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+	
+	if ((g->v = malloc(n * sizeof(edge_bucket_t *))) == NULL) {
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	for (i = 0; i < n; i++)
+		g->v[i] = malloc(sizeof(edge_bucket_t));
+
+	return g;
+}
+
+int main(void)
+{
+	graph_t *g = graph_create(10);
+
+	g->v[0]->outedges[0] = 1;
+	g->v[0]->outedges[1] = 2;
+	g->v[0]->outedges[2] = 3;
+
+	return 0;
+}
